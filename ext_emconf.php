ff5906376175c06ed801812555289273df5a9c5a
tillko <tillko@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-07 11:36:15 +0000

    updated manual
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mouseoverimages/trunk@15531 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "mouseoverimages"
 #
-# Auto generated 26-10-2008 11:13
+# Auto generated 07-01-2009 12:03
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.2.4',
+	'version' => '0.2.5',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"f79e";s:10:"README.txt";s:4:"befe";s:12:"ext_icon.gif";s:4:"3c00";s:17:"ext_localconf.php";s:4:"929a";s:14:"ext_tables.php";s:4:"f51e";s:14:"ext_tables.sql";s:4:"4695";s:16:"locallang_db.xml";s:4:"a3ec";s:36:"pi1/class.tx_mouseoverimages_pi1.php";s:4:"4ee7";s:15:"css/default.css";s:4:"8487";s:12:"css/hover.js";s:4:"35d0";s:11:"css/ie7.css";s:4:"fcce";s:14:"doc/manual.sxw";s:4:"1c56";s:19:"doc/wizard_form.dat";s:4:"54cb";s:20:"doc/wizard_form.html";s:4:"59bf";s:38:"static/Mouse_over_Images/constants.txt";s:4:"beee";s:34:"static/Mouse_over_Images/setup.txt";s:4:"9df7";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"1f0c";s:10:"README.txt";s:4:"befe";s:12:"ext_icon.gif";s:4:"3c00";s:17:"ext_localconf.php";s:4:"929a";s:14:"ext_tables.php";s:4:"f51e";s:14:"ext_tables.sql";s:4:"4695";s:16:"locallang_db.xml";s:4:"a3ec";s:36:"pi1/class.tx_mouseoverimages_pi1.php";s:4:"4ee7";s:15:"css/default.css";s:4:"8487";s:12:"css/hover.js";s:4:"35d0";s:11:"css/ie7.css";s:4:"fcce";s:14:"doc/manual.sxw";s:4:"760f";s:19:"doc/wizard_form.dat";s:4:"54cb";s:20:"doc/wizard_form.html";s:4:"59bf";s:38:"static/Mouse_over_Images/constants.txt";s:4:"beee";s:34:"static/Mouse_over_Images/setup.txt";s:4:"9df7";}',
 	'suggests' => array(
 	),
 );
